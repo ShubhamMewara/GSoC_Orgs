ae47321c408c0f91e2bd565ebc7fd367bfc1ceba
ShubhamMewara <shubhammewara12@gmail.com>
2024-03-13 21:32:52 +0530

    vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
           >
             <NavbarContextProvider>
             {children}
+            <SpeedInsights />
             <Analytics />
             </NavbarContextProvider>
           </ThemeProvider>
